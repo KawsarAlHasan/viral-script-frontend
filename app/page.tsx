104e34b377f420c2515b0dde322ae654cccb5d56
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2025-06-28 12:06:19 +0600

    check 7

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,14 @@ import Footer from "./_components/Footer";
 import LoginModal from "./_components/LoginModal";
 import AIScriptSection from "./_components/AIScriptSection";
 
+import Navbar2 from './_components/Navbar2'
+
 export default function Home() {
   return (
-   <div className="min-h-screen bg-[#ffffff] dark:bg-[#0a0a0a] text-[#171717] dark:text-[#ededed] transition-colors duration-300 mt-[82px]">
- 
+   <div className="min-h-screen bg-[var(--background)] text-[var(--foreground)] transition-colors duration-300 mt-[82px]">
+    
+    <Navbar2 />
+    
     <Header/>
 
     <ScriptSection/>
