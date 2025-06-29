e3f0c79416c09460f3a5cdbbb80f04a72d48eca7
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2025-06-29 09:58:50 +0600

    theme bug fixed

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import ThemeSwitcher from "../_components/Theme";
 
 const Header = () => {
   return (
   <div>
 
       <div className="min-h-screen theme-container">
-    <ThemeSwitcher />
       hello world
       <button className="c-button">Click</button>
     </div>
